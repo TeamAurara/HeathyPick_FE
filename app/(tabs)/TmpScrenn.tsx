f6159a3e88f3d3120b879773173a5aed2518b4d1
Choi Yong Won <cywin0821@gmail.com>
2025-06-22 16:36:27 +0900

    chore : 초기 페이지 추가

---
+++ app/(tabs)/TmpScrenn.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function TmpScreen() {
+  return (
+    <View className="flex-1 justify-center items-center bg-blue-500">
+      <Text className="text-white text-xl">Hello, TmpScreen!</Text>
+    </View>
+  );
+}
