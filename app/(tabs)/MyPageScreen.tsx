a49d7ffc54ce92d3bb9377a0f630d297c56581d2
Choi Yong Won <cywin0821@gmail.com>
2025-06-22 17:10:35 +0900

    chore : 마이페이지 화면으로 수정

---
+++ app/(tabs)/MyPageScreen.tsx
@@ -0,0 +1,14 @@
+import { Stack } from "expo-router";
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function MyPageScreen() {
+    return (
+        <>
+            <Stack.Screen options={{ title: "마이페이지", headerShown: false }} />
+            <View className="flex-1 justify-center items-center bg-blue-500">
+                <Text className="text-white text-xl">Hello, MyPageScreen!</Text>
+            </View>
+        </>
+    );
+}
