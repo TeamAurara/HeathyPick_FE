6d68d864144efbb3833d173abf4c7de1aa6ec7e4
Choi Yong Won <cywin0821@gmail.com>
2025-06-28 15:38:57 +0900

    chore [#2] : 카카오 맵을 위한 컴포넌트 추가

---
+++ components/map/KakaoMap.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const KakaoMap = () => {
+    return (
+        <View>
+            <Text>KakaoMap</Text>
+        </View>
+    )
+}
+
+export default KakaoMap;
