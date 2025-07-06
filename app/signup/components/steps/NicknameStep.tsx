040abf6ca63a9330894656d3b4a0d16132717294
Choi Yong Won <cywin0821@gmail.com>
2025-07-06 16:03:27 +0900

    feat : [#5] 닉네임 스텝 ui 부분 수정

--- app/signup/components/steps/NicknameStep.tsx
+++ app/signup/components/steps/NicknameStep.tsx
@@ -10,10 +10,10 @@ interface NicknameStepProps {
 const NicknameStep = ({ nickname, setNickname }: NicknameStepProps) => {
   return (
     <View className="w-full">
-      <Text className="text-3xl font-bold text-center mb-6">
+      <Text className="text-3xl font-bold text-center mb-6 mt-10">
         만나서 반가워요!{'\n'}어떻게 불러드릴까요?
       </Text>
-      <Text className="text-green-600 font-medium mb-2">닉네임</Text>
+      <Text className="text-green-600 font-medium mb-2 mt-10">닉네임</Text>
       <InputWithClearButton
         value={nickname}
         onChangeText={setNickname}
