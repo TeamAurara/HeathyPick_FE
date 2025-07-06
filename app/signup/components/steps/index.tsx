be4225ccad55b2f9707e2cc11d1681c633fdfc8e
Choi Yong Won <cywin0821@gmail.com>
2025-07-06 15:03:07 +0900

    chore : [#5] 마지막 성공페이지를 추가 하기 위해 컴포넌트  추가

--- app/signup/components/steps/index.tsx
+++ app/signup/components/steps/index.tsx
@@ -6,6 +6,7 @@ export { default as AgeStep } from './AgeStep';
 export { default as BodyInfoStep } from './BodyInfoStep';
 export { default as GenderStep } from './GenderStep';
 export { default as NicknameStep } from './NicknameStep';
+export { default as SuccessStep } from './SuccessStep';
 
 // 기본 내보내기를 위한 빈 컴포넌트
 const StepComponents: React.FC = () => {
